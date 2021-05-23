4aed7455fbb5098e58adfc2a7b9841b1d045c334
jiayouba233 <72449367+jiayouba233@users.noreply.github.com>
2021-05-23 12:25:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy.netptop.workers.dev',
+    serverName: 'siteproxy-9by0wcm1c-jiayouba233.vercel.app',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
